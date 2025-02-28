52133378739a6c84897793a65e033e0bc2f84982
ESCUDERO PLAZA MARIA DEL ROSARIO <mescuderop@msuteq.edu.ec>
2025-02-28 01:03:57 -0500

    modifique form2

--- Sga/Form2.Designer.cs
+++ Sga/Form2.Designer.cs
@@ -49,6 +49,7 @@
             // 
             // txtBox_G_cédulaAlumno
             // 
+            this.txtBox_G_cédulaAlumno.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.txtBox_G_cédulaAlumno.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(236)))), ((int)(((byte)(221)))));
             this.txtBox_G_cédulaAlumno.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtBox_G_cédulaAlumno.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -61,6 +62,7 @@
             // 
             // txtBox_G_nombresAlumno
             // 
+            this.txtBox_G_nombresAlumno.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.txtBox_G_nombresAlumno.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(236)))), ((int)(((byte)(221)))));
             this.txtBox_G_nombresAlumno.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtBox_G_nombresAlumno.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -74,6 +76,7 @@
             // 
             // lbnConfirmar
             // 
+            this.lbnConfirmar.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.lbnConfirmar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(236)))), ((int)(((byte)(221)))));
             this.lbnConfirmar.FlatAppearance.BorderSize = 0;
             this.lbnConfirmar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
@@ -91,6 +94,7 @@
             // 
             // lbnEliminar
             // 
+            this.lbnEliminar.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.lbnEliminar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(236)))), ((int)(((byte)(221)))));
             this.lbnEliminar.FlatAppearance.BorderSize = 0;
             this.lbnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
@@ -108,6 +112,7 @@
             // 
             // lblCedula
             // 
+            this.lblCedula.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.lblCedula.AutoSize = true;
             this.lblCedula.BackColor = System.Drawing.Color.Transparent;
             this.lblCedula.Font = new System.Drawing.Font("Times New Roman", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -120,6 +125,7 @@
             // 
             // label1
             // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -208,6 +214,7 @@
             // 
             // label2
             // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -220,6 +227,7 @@
             // 
             // label4
             // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Times New Roman", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -232,6 +240,7 @@
             // 
             // label5
             // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.Color.Transparent;
             this.label5.Font = new System.Drawing.Font("Times New Roman", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -244,6 +253,7 @@
             // 
             // label6
             // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.label6.AutoSize = true;
             this.label6.BackColor = System.Drawing.Color.Transparent;
             this.label6.Font = new System.Drawing.Font("Times New Roman", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
